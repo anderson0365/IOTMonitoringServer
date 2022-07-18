0b0ff82fa770bdb169bf8d0a63bf3e28a8bcc697
Anderson Luis Cáceres Padilla <al.caceres@uniandes.edu.co>
2022-07-18 05:49:22 -0500

    IPs

--- IOTMonitoringServer/settings.py
+++ IOTMonitoringServer/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-bu+)8ft@9+qd*#e#f_s@wkyv2tmq+#!a^3j15h3kjk^jzksu0j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "52.91.197.142"]
+ALLOWED_HOSTS = ["localhost", "3.80.225.42"]
 
 
 # Application definition
@@ -95,7 +95,7 @@ DATABASES = {
         "NAME": "iot_data",  # Nombre de la base de datos
         "USER": "dbadmin",  # Nombre de usuario
         "PASSWORD": "uniandesIOT1234*",  # Contraseña
-        "HOST": "54.197.194.233",  # Dirección IP de la base de datos
+        "HOST": "34.233.126.100",  # Dirección IP de la base de datos
         "PORT": "",  # Puerto de la base de datos
     }
 }
@@ -155,7 +155,7 @@ LOGOUT_REDIRECT_URL = '/login/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Dirección del bróker MQTT
-MQTT_HOST = "34.229.162.158"
+MQTT_HOST = "3.80.45.154"
 
 # Puerto del bróker MQTT
 MQTT_PORT = 8082
