825e213e0c6961806fc4a3b0ada18b1fdc170dbb
Anderson Luis Cáceres Padilla <al.caceres@uniandes.edu.co>
2022-07-18 07:16:02 -0500

    Fast commit

--- control/monitor.py
+++ control/monitor.py
@@ -18,7 +18,7 @@ def analyze_data():
     basetime = datetime.now()
     print(f"Calculando alertas...{basetime}")
     data = Data.objects.filter(
-        base_time__gte=basetime- timedelta(minutes=2))
+        base_time__gte=basetime- timedelta(minutes=10))
     print(len(data))
     aggregation = data.annotate(check_value=Avg('avg_value')) \
         .select_related('station', 'measurement') \
